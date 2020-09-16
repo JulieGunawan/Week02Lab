b2ada2167d4619aecdcc9cb9f5e885456d91c5ec
751682 <751682@SAIT224907IT.ACDM.DS.SAIT.CA>
2020-09-16 08:30:48 -0600

    Printing Hello World Rev1

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -15,7 +15,7 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World part 2");
     }
     
 }
